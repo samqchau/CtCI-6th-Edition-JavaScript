b2d8486cc8b002c06a6ad59b7e9caf1557a90be3
samqchau <67344952+samqchau@users.noreply.github.com>
2021-01-25 18:01:40 -0800

    1.1 & 1.2

--- chapter01/1.1 - Is Unique/isUnique.js
+++ chapter01/1.1 - Is Unique/isUnique.js
@@ -1,42 +1,89 @@
-var allUniqueChars = function(string) {
-  
-  // O(n^2) approach, no additional data structures used
-  // for each character, check remaining characters for duplicates
-  for (var i = 0; i < string.length; i++) {
-    for (var j = i + 1; j < string.length; j++) {
-      if (string[i] === string[j]) {
-        return false; // if match, return false
+// var allUniqueChars = function (string) {
+//   // O(n^2) approach, no additional data structures used
+//   // for each character, check remaining characters for duplicates
+//   for (var i = 0; i < string.length; i++) {
+//     for (var j = i + 1; j < string.length; j++) {
+//       if (string[i] === string[j]) {
+//         return false; // if match, return false
+//       }
+//     }
+//   }
+//   return true; // if no match, return true
+// };
+
+// const everyCharUnique = (str, indexOffset = 'a'.charCodeAt()) => {
+//   let counterTable = Number();
+//   for (let index of [...str].map((c) => c.charCodeAt() - indexOffset)) {
+//     const mask = 1 << index;
+//     if (counterTable & mask) return false;
+//     counterTable |= mask;
+//   }
+//   return true;
+// };
+
+// function everyCharUnique(str) {
+//   let obj = {};
+//   for (let i = 0; i < str.length; i++) {
+//     if (obj[str[i]] && obj[str[i]] >= 1) {
+//       return false;
+//     } else {
+//       obj[str[i]] = 1;
+//     }
+//   }
+//   return true;
+// }
+
+//Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
+//O(n^2) run time. Nested for loops. Brute force. For every character in the string, it checks the rest of the string to see if the char pair matches. If they do, returns false. Returns true after checking every pair.
+function hasUniqueCharacters(str) {
+  for (i = 0; i < str.length; i++) {
+    for (j = i + 1; j < str.length; j++) {
+      if (str[i] === str[j]) {
+        return false;
       }
     }
   }
-  return true; // if no match, return true
-};
+  return true;
+}
 
-const everyCharUnique = (str, indexOffset = 'a'.charCodeAt()) => {
-    let counterTable = Number();
-    for(let index of [...str].map(c => c.charCodeAt() - indexOffset)) {
-        const mask = 1 << index;
-        if(counterTable & mask)
-            return false;
-        counterTable |= mask;
-    }
-    return true;
-};
-
-function everyCharUnique(str) {
-  let obj = {};
-  for (let i = 0; i < str.length; i++) {
-    if (obj[str[i]] && obj[str[i]] >= 1) {
+//O(n) runtime. Iterates through the string a single time. As iterating through the string, adds char to hash table. If the key value pair already exists, returns false, else keeps going. After iterating through the whole string, returns true.
+function uniqueCharsHash(str) {
+  //iterate through string and add chars to hash table, if key value pair already exists, return false
+  let myobj = {};
+  for (i = 0; i < str.length; i++) {
+    if (myobj[str[i]]) {
       return false;
     } else {
-      obj[str[i]] = 1;
+      myobj[str[i]] = 1;
+    }
+  }
+  return true;
+}
+
+//O(nlogn) runtime.
+function sortAndSearch(str) {
+  let sorted = [...str].sort((a, b) => (a > b ? 1 : -1)).join('');
+  for (i = 1; i < str.length; i++) {
+    if (sorted[i] === sorted[i - 1]) {
+      return false;
     }
   }
   return true;
 }
 
 /* TESTS */
-console.log(everyCharUnique('abcd'), 'true');
-console.log(everyCharUnique('abccd'), 'false');
-console.log(everyCharUnique('bhjjb'), 'false');
-console.log(everyCharUnique('mdjq'), 'true');
+console.log(hasUniqueCharacters('abcd'), 'true');
+console.log(hasUniqueCharacters('abccd'), 'false');
+console.log(hasUniqueCharacters('bhjjb'), 'false');
+console.log(hasUniqueCharacters('mdjq'), 'true');
+
+console.log(uniqueCharsHash('abcd'), 'true');
+console.log(uniqueCharsHash('abccd'), 'false');
+console.log(uniqueCharsHash('bhjjb'), 'false');
+console.log(uniqueCharsHash('mdjq'), 'true');
+
+console.log(sortAndSearch('abcd'), 'true');
+console.log(sortAndSearch('abccd'), 'false');
+console.log(sortAndSearch('bhjjb'), 'false');
+console.log(sortAndSearch('mdjq'), 'true');
--- chapter01/1.2 - Check Perm/checkPermute.js
+++ chapter01/1.2 - Check Perm/checkPermute.js
@@ -1,14 +1,48 @@
-var checkPermute = function(stringOne, stringTwo) {
-  // if different lengths, return false
-  if (stringOne.length !== stringTwo.length) {
+// var checkPermute = function(stringOne, stringTwo) {
+//   // if different lengths, return false
+//   if (stringOne.length !== stringTwo.length) {
+//     return false;
+//   // else sort and compare
+//   // (doesnt matter how it's sorted, as long as it's sorted the same way)
+//   } else {
+//     var sortedStringOne = stringOne.split('').sort().join('');
+//     var sortedStringTwo = stringTwo.split('').sort().join('');
+//     return sortedStringOne === sortedStringTwo;
+//   }
+// };
+
+//Check Permutation: Given two strings, write a method to decide if one is apermutation of the other.
+
+//O(a) runtime. Checks if they are the same length. If they are not the same length, returns false. Iterates through string a and counts frequency of every char in string a and stores in hash table. Iterates through b and decrements values in hash table. If key value pair doesn't exist or if value is less than 0 after decrementing, returns false. After iterating through both strings, returns true.
+function checkPermute(a, b) {
+  if (a.length !== b.length) {
     return false;
-  // else sort and compare 
-  // (doesnt matter how it's sorted, as long as it's sorted the same way)
-  } else {
-    var sortedStringOne = stringOne.split('').sort().join('');
-    var sortedStringTwo = stringTwo.split('').sort().join('');
-    return sortedStringOne === sortedStringTwo;
   }
+  let map = {};
+  for (i = 0; i < a.length; i++) {
+    map[a[i]] ? (map[a[i]] += 1) : (map[a[i]] = 1);
+  }
+  console.log(map);
+  for (i = 0; i < b.length; i++) {
+    if (!map[b[i]]) {
+      return false;
+    }
+    map[b[i]] -= 1;
+    if (map[b[i]] < 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//O(aloga + blogb) converts a and b to arrays and sorts them (nlogn). Converts back to strings and returns whether they are equal or not.
+const sortCheckPerm = (a, b) => {
+  if (a.length !== b.length) {
+    return false;
+  }
+  const aarr = [...a].sort().join(',');
+  const barr = [...b].sort().join(',');
+  return aarr === barr;
 };
 
 // Tests
@@ -17,3 +51,9 @@ console.log(checkPermute('aba', 'aab'), true);
 console.log(checkPermute('aba', 'aaba'), false);
 
 console.log(checkPermute('aba', 'aa'), false);
+
+console.log(sortCheckPerm('aba', 'aab'), true);
+
+console.log(sortCheckPerm('aba', 'aaba'), false);
+
+console.log(sortCheckPerm('aba', 'aa'), false);
